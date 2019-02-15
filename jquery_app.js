b102bfc11697b560e5b3bb3ddf8c3b22fb560252
Jace Gold <jacegold@gmail.com>
2019-02-15 15:38:32 -0700

    Remove IDs from index_jquery.html, completed jQuery

--- jquery_app.js
+++ jquery_app.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  alert('jQuery Working');
+  function changeImg(img, caption) {
+    $('#image').attr({
+      'src': 'images/' + img.toLowerCase() + '.jpg',
+      'alt': caption,
+    });
+    
+    $("#caption").text(caption);
+  }
+
+  $("#select-btns").click(function (e) {
+    // This will show us the element so we can destructure it
+    // to get the values needed
+    console.table(e.target);
+    changeImg(e.target.innerText, e.target.dataset.caption);
+  });
 });
